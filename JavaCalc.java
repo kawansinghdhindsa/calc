c04a6b456637a5e5b3d957df4a2d33a8863b62a2
kawansinghdhindsa <kawansdhindsa+github@gmail.com>
2026-03-09 10:23:26 -0700

    created a basic calculator

---
+++ JavaCalc.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class JavaCalc {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("What is A:");
+        double a = input.nextDouble();
+        System.out.printf("What is B:");
+        double b = input.nextDouble();
+        System.out.printf("operator");
+        String operator = input.next();
+        switch (operator) {
+            case "+":
+            case "plus":
+                System.out.printf("%.2f + %.2f = %.2f",a,b,a+b);
+                break;
+            
+            case "-":
+            case "minus":
+                System.out.printf("%.2f - %.2f = %.2f",a,b,a-b);
+                break;
+            
+            case "x":
+            case "*":
+            case "times":
+            case "multiply":
+                System.out.printf("%.2f * %.2f = %.2f",a,b,a*b);
+                break;
+            
+            case "/":
+            case "divide":
+                System.out.printf("%.2f + %.2f = %.2f",a,b,a/b);
+                break;
+        
+            default:
+                System.out.println("operator not recognized");
+                return;
+        }
+    }
+}
+
